1b711a242864c70fa80158f771a6612366c14676
Anuradha K <anuradha.ittech@gmail.com>
2024-07-14 12:12:10 -0500

    Created ReadData.java

---
+++ basics/ReadData.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ReadData {
+
+	public static void main(String args[]) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+		System.out.println("You entered:"+n);
+		sc.close();
+	  }
+}
